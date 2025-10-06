2e215b6e2cb74843e208f6b486e0f498a09a439e
7s.i <7s.i@pm.me>
2025-10-07 00:10:12 +0800

    刪除不再使用的路由和組件，更新用戶登錄密碼提示以反映正確的密碼格式，簡化專案結構並增強用戶體驗。

--- src/app/routes/alias/alias.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', loadComponent: () => import('./owner-router.component').then(m => m.OwnerRouterComponent) },
-  { path: ':projectSlug', loadComponent: () => import('./owner-router.component').then(m => m.OwnerRouterComponent) }
-];
-
-
